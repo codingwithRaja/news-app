48d03328827a9b279e848979b397b6e6458041c9
codingwithRaja <rjali40@gmail.com>
2023-03-09 19:16:15 +0500

    News Website Continue

---
+++ src/Components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is News component
+        
+      </div>
+    )
+  }
+}
+
+export default News
