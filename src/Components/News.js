1befd8e45a2a0714319d74c6c06674d5ff3f0b19
codingwithRaja <rjali40@gmail.com>
2023-03-10 21:36:08 +0500

    done

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is News component
-        
-      </div>
-    )
-  }
+
+    constructor() {
+        super();
+        // console.log("Hello I am constructor from News JS")
+        this.state = {
+            articles: [],
+            loading: false
+        }
+    }
+    async componentDidMount() {
+        // console.log("cdm")
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d0e27dbfa2db47c0a52b1aeca6013224"
+        let data = await fetch(url)
+        let parseData = await data.json()
+        console.log(parseData)
+        this.setState({ articles: parseData.articles, description: parseData.description, imageUrl: parseData.urlToImage })
+    }
+    render() {
+        // console.log("render")
+        return (
+            <div className='container my-3'>
+                <h2>MongoNews - Top HEADLINES</h2>
+                <div className='row '>
+                    {this.state.articles.map((element) => {
+
+                        return <div className='col-md-4' key={element.url}>
+                            <NewsItem title={element.title ? element.title.slice(0, 40) : ""}
+                                description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+
+
+
+                </div>
+
+
+
+            </div>
+        )
+    }
 }
 
 export default News
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props
+        return (
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl ? "https://media.cnn.com/api/v1/images/stellar/prod/230309120312-climeworks-direct-air-capture-plant-file-090721.jpg?c=16x9&q=w_800,c_fill" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-success">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
